4c21c3ab0c1e0bc29c6dd7897d2a4e081fbb46fa
AndreyAvila <87436209+AndreyAvila@users.noreply.github.com>
2021-07-14 11:04:19 -0500

    Update SOAT_T.py

--- SOAT_T.py
+++ SOAT_T.py
@@ -21,11 +21,9 @@ ruta = 'Tarifario SOAT 202102.xlsx'
 t_digital = pd.read_excel(ruta,sheet_name= "TD_Crecer_M",header = 7, index_col =[0,1,2],engine ='openpyxl' )
 t_fisico = pd.read_excel(ruta,sheet_name= "TF_Crecer_M",header = 7, index_col =[0,1,2] ,engine ='openpyxl')
 
-#t_digital["llave"] = t_digital.index.copy()
-t_digital["llave"] = t_digital.multiindex.to_numpy()
+t_digital["llave"] = t_digital.index.copy()
 t_digital.set_index("llave",append= True , inplace = True)
-#t_fisico["llave"] = t_fisico.index.copy()
-t_fisico["llave"] = t_fisico.multiindex.to_numpy()
+t_fisico["llave"] = t_fisico.index.copy()
 t_fisico.set_index("llave",append= True , inplace = True)
 
 
