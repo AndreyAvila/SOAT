da2704e50b46d00a9038440c7b08b3da19c75252
AndreyAvila <87436209+AndreyAvila@users.noreply.github.com>
2021-07-14 10:49:04 -0500

    Update SOAT_T.py

--- SOAT_T.py
+++ SOAT_T.py
@@ -5,8 +5,6 @@ Created on Wed Feb 24 08:31:20 2021
 @author: EFERNANDEZ
 """
 
-python.pythonPath": "${workspaceFolder}/env/bin/python3.6",
-git.ignoreLimitWarning": true
        
 import streamlit as st
 import pandas as pd
